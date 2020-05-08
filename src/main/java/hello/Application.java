9e1551c431df449cdf91e84e886642cefca060e1
P Campbell <pcampbell.edu@gmail.com>
2020-05-08 17:43:33 -0400

    add date to Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 
 @SpringBootApplication
 @RestController
@@ -11,7 +14,13 @@ public class Application {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello Docker World coming to you from Spring Boot!<br>Update the repo<br> testing webhook to jenkins";
+	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	Date date = new Date();
+	String display_date = dateFormat.format(date);
+
+    return "Hello Docker World coming to you from Spring Boot!<br>Update the repo<br> testing webhook to jenkins<br>"
+		+display_date	;
+
   }
 
   public static void main(String[] args) {
